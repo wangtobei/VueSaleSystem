434e284ae005768e1e9079c1e74a8e4e47f59d02
wangbeibei <1498580056@qq.com>
2020-12-10 13:30:17 +0800

    done

--- src/base.config.js
+++ src/base.config.js
@@ -1,1 +1,5 @@
 export const AXIOS_BASE_URL = "http://localhost:8080";
+//export const AXIOS_BASE_URL = "http://192.168.172.131:8000";
+
+//修改hosts文件确保谷歌验证码可以加载出来
+//export const AXIOS_BASE_URL = "http://www.springmvc.com:8000";
